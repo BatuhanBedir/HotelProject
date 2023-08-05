982c1b6729f68faabcbbef2986f49920323977f9
BatuhanBedir <114083704+BatuhanBedir@users.noreply.github.com>
2023-08-05 17:41:25 +0300

    pageedited

---
+++ ApiConsume/HotelProject.DataAccessLayer/Migrations/20230805141749_add_guest.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HotelProject.DataAccessLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class add_guest : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Bookings",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateTable(
+                name: "Guests",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Surname = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    City = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Guests", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Guests");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Bookings",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
---
+++ ApiConsume/HotelProject.EntityLayer/Concrete/Guest.cs
@@ -0,0 +1,10 @@
+﻿using HotelProject.EntityLayer.Abstract;
+
+namespace HotelProject.EntityLayer.Concrete;
+
+public class Guest : BaseEntity
+{
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public string City { get; set; }
+}
