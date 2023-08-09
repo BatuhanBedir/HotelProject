9215a3f2a4ce21f349f5b8d5f78e1a525fc0adab
BatuhanBedir <114083704+BatuhanBedir@users.noreply.github.com>
2023-08-09 14:15:20 +0300

    mail/file/aut

---
+++ Frontend/HotelProject.WebUI/Controllers/AdminMailController.cs
@@ -0,0 +1,42 @@
+﻿using HotelProject.WebUI.Models.Mail;
+using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+
+namespace HotelProject.WebUI.Controllers;
+
+public class AdminMailController : Controller
+{
+    [HttpGet]
+    public IActionResult Index()
+    {
+        return View();
+    }
+    [HttpPost]
+    public IActionResult Index(AdminMailVm model)
+    {
+        MimeMessage mimeMessage = new MimeMessage();
+
+        //kimden
+        MailboxAddress mailboxAddressFrom = new MailboxAddress("HotelierAdmin", "batuhan.bedir133@gmail.com");
+        mimeMessage.From.Add(mailboxAddressFrom);
+
+        //kime
+        MailboxAddress mailboxAddressTo = new MailboxAddress("User", model.ReceiverMail);
+        mimeMessage.To.Add(mailboxAddressTo);
+
+        //mesaj içeriği
+        var bodyBuilder = new BodyBuilder();
+        bodyBuilder.TextBody = model.Body;
+        mimeMessage.Body = bodyBuilder.ToMessageBody();
+        mimeMessage.Subject = model.Subject;
+
+        SmtpClient client = new SmtpClient();
+        client.Connect("smtp.gmail.com", 587,false);
+        client.Authenticate("batuhan.bedir133@gmail.com", "tybhixzbrcwuydqq");
+        client.Send(mimeMessage);
+        client.Disconnect(true);
+
+        return View();
+    }
+}
--- Frontend/HotelProject.WebUI/Controllers/RegisterController.cs
+++ Frontend/HotelProject.WebUI/Controllers/RegisterController.cs
@@ -1,43 +1,43 @@
 ﻿using HotelProject.EntityLayer.Concrete;
 using HotelProject.WebUI.Dtos.RegisterDto;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
-namespace HotelProject.WebUI.Controllers
+namespace HotelProject.WebUI.Controllers;
+[AllowAnonymous]
+public class RegisterController : Controller
 {
-    public class RegisterController : Controller
+    private readonly UserManager<AppUser> _userManager;
+
+    public RegisterController(UserManager<AppUser> userManager)
     {
-        private readonly UserManager<AppUser> _userManager;
+        _userManager = userManager;
+    }
 
-        public RegisterController(UserManager<AppUser> userManager)
-        {
-            _userManager = userManager;
-        }
+    [HttpGet]
+    public IActionResult Index()
+    {
+        return View();
+    }
 
-        [HttpGet]
-        public IActionResult Index()
+    [HttpPost]
+    public async Task<IActionResult> Index(CreateNewUserDto createNewUserDto)
+    {
+        if (!ModelState.IsValid) { return View(); }
+        var appUser = new AppUser()
         {
-            return View();
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> Index(CreateNewUserDto createNewUserDto)
+            Name = createNewUserDto.Name,
+            Surname = createNewUserDto.Surname,
+            Email = createNewUserDto.Mail,
+            UserName = createNewUserDto.Username,
+            City = createNewUserDto.City,
+        };
+        var result = await _userManager.CreateAsync(appUser, createNewUserDto.Password);
+        if (result.Succeeded)
         {
-            if (!ModelState.IsValid) { return View(); }
-            var appUser = new AppUser()
-            {
-                Name = createNewUserDto.Name,
-                Surname = createNewUserDto.Surname,
-                Email = createNewUserDto.Mail,
-                UserName = createNewUserDto.Username,
-                City = createNewUserDto.City,
-            };
-            var result = await _userManager.CreateAsync(appUser, createNewUserDto.Password);
-            if (result.Succeeded)
-            {
-                return RedirectToAction("Index", "Login");
-            }
-            return View();
+            return RedirectToAction("Index", "Login");
         }
+        return View();
     }
 }
